1461a0d61a8cc103021ef76389b67d174b286eb0
Michael-Kochis <rhishisikk@gmail.com>
2021-07-06 10:37:10 -0500

    basic scaffolding

--- api/posts/posts-router.js
+++ api/posts/posts-router.js
@@ -1,1 +1,41 @@
 // implement your posts router here
+const express = require('express');
+const posts = require('./posts-model');
+
+const router = express.Router();
+
+//get all
+router.get("/", (req, res) => {
+    
+})
+
+//get by ID
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    
+})
+
+//post
+router.get("/", (req, res) => {
+    
+})
+
+//put
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    
+})
+
+//delete
+router.get("/:id", (req, res) => {
+    const { id } = req.params;
+    
+})
+
+//get comments
+router.get("/:id/comments", (req, res) => {
+    const { id } = req.params;
+    
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,2 +1,8 @@
 // implement your server here
 // require your posts router and connect it here
+// imports
+const express = require('express');
+const cors = require('cors');
+//const db = require('./posts/posts-router');
+
+module.exports = server;
